f34ae7402671ad0db7311ea06195a5f0afd75467
SARamanujan <SA_Ramanujan@yahoo.co.jp>
2016-09-07 09:17:35 +0900

    graph_0001.py

---
+++ src/graph_0001.py
@@ -0,0 +1,15 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*
+'''graph_0001
+'''
+
+import sys, os
+import numpy as np
+from matplotlib import pyplot as plt
+
+def main():
+  ax = plt.figure()
+  plt.show()
+
+if __name__ == '__main__':
+  main()
